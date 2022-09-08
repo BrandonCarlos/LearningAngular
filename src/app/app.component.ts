5246573324da4f1b5cfa337423edbc86c909838b
BrandonCarlos <brandon_marzo@hotmail.com>
2022-09-08 15:45:53 -0300

    Export data with output, Receive data in component, interpolating data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any;
+
+  transferir($event) {
+    console.log($event);//ta mostrando = {valor = 432, destino = 1312}
+    this.transferencia = $event; //ou seja to mandando p/ o app.component.html o objeto com os atributos "valor e destino"
+  }
 }
--- src/app/nova transferencia/nova-transferencia.component.ts
+++ src/app/nova transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Output,EventEmitter } from "@angular/core";
 
 @Component({//Está é minha classe, e é aqui que vou colocar os métodos de click, submit, etc...
     selector: 'app-nova-transferencia',
@@ -9,14 +9,23 @@ import { Component } from "@angular/core";
 export class NovaTransferenciaComponent {//Aqui que vai ter nossas lógicas e nossos métodos, como está CLASS é export, então temos váriaveis
   //que podem ser acessadas fora desta classe em outro arquivo
 
-  valor: number = 12;
-  destino: number = 24;
+  //mandar estes dados para quem o invocou, tirar esses dados para fora enviar para quem chamou
+  @Output()
+  aoTransferir: EventEmitter<any> = new EventEmitter<any>(); //esse evento "aoTransferir" vai poder receber qualquer tipo de dados, objeto, number etc..
 
+  valor: number;
+  destino: number;
 
   transferir() {
     console.log('Solciitado nova transferencia');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);//após emitir os dados, abaixo vamos limpar os campos
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
   }
 
 }
